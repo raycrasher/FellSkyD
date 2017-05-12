aa06ebdc9615f31fdcc54916785a981f184574ec
Ian David Garcia <cinquedia@gmail.com>
2017-05-12 16:41:23 +0800

    Added AdvAnimatedSpriteRenderer placeholder

---
+++ Source/Code/CorePlugin/Components/AdvAnimatedSpriteRenderer.cs
@@ -0,0 +1,24 @@
+﻿using Duality.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Duality.Drawing;
+using Duality;
+using FellSky.Resources;
+
+namespace FellSky.Components
+{
+    [Duality.Editor.EditorHintCategory("Graphics")]
+    public class AdvAnimatedSpriteRenderer : Renderer
+    {
+        public ContentRef<Sprite>[] Frames { get; set; }
+        public override float BoundRadius => throw new NotImplementedException();
+
+        public override void Draw(IDrawDevice device)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
