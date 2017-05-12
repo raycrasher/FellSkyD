b4945b223d65aca82eca12cb083ad73bdbeae069
Ian David Garcia <cinquedia@gmail.com>
2017-05-12 15:34:16 +0800

    Added working TexturedFrame component for use by UI controls.

---
+++ Data/Gui/Frame.Pixmap.res
@@ -0,0 +1,26 @@
+﻿<root dataType="Struct" type="Duality.Resources.Pixmap" id="129723834">
+  <animCols dataType="Int">0</animCols>
+  <animFrameBorder dataType="Int">0</animFrameBorder>
+  <animRows dataType="Int">0</animRows>
+  <assetInfo dataType="Struct" type="Duality.Editor.AssetManagement.AssetInfo" id="427169525">
+    <customData />
+    <importerId dataType="String">BasicPixmapAssetImporter</importerId>
+    <sourceFileHint dataType="Array" type="System.String[]" id="1100841590">
+      <item dataType="String">{Name}.png</item>
+    </sourceFileHint>
+  </assetInfo>
+  <atlas />
+  <layers dataType="Struct" type="System.Collections.Generic.List`1[[Duality.Drawing.PixelData]]" id="2035693768">
+    <_items dataType="Array" type="Duality.Drawing.PixelData[]" id="2696347487" length="4">
+      <item dataType="Struct" type="Duality.Drawing.PixelData" id="1485019246" custom="true">
+        <body>
+          <version dataType="Int">4</version>
+          <formatId dataType="String">image/png</formatId>
+          <pixelData dataType="Array" type="System.Byte[]" id="2204481104">iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAnISURBVHhe5ZM90p1XEYRv4iWwBCJCFYsgp1gCVSRksACWQADllZiYokiAInFAZAnJEpYs68f6sSXL/nx6PM+oZ+55r2RCSVVdM9PdM/PO0f1OV6dT4PHPrp0+/sOHp4/+c/VO488f//P0q3/8+vSTv54CPMCVsAzvBdbh4If/+ff1AX7+92snfvYhrH9/KXz3Ov/O+QvcEaaXOmLu2c6zb9hC+ps8gvm0h3t//+8PT/xtvH4AM0Zt3FnuteLUkvdZ89CqD3pnPz44xchHf+sZ4F7d3h+AITbs0qDC/MgF6iO+YewLjx/k+cB2nmPzbdyr289/ARu0AXyM4oUPu6gd4WD2G48cOPPnPHju1e1v9QBHH9a0RCyZHvgjmH/r28w74tVfMw76uFe3bx/g0se6Rn7JH5gHrprY+Dfg0HNwqMN7uVe3/7hfQKJ9SGpw24/Mgx3weLZ9bwL94/sEn0dO5F7dfv4A+bGR77BZtsXwaSZw/WjX9P0/OJrNvbr97X4BGxwNL9iHv/HIt8GPeIjtXOvnXt3eH2CZaD4bYho4XPQ2H4tv552c1xv/9jsugHt1+/EvgEUZ28OY9p1/kHJxw0cd3CbWvOSCt96aCUdtKH9q1UtuPdyr29sDhMGMDJl1fVDmXnv/t9TmCW6HOQNMT+LbERum1+uVc69ubw8gYw3+NmOCvDh5lyeGp1cRLT4i9dkfefLByYfX+NIXXjmnfbnL+Vfssr07cK9u7w8gw2pmaH1Y1pGjZySX3j5mxPJYP14ArwiipjfryNPn/tCy/kY1XOp4uFe3twfQUDWGWbmGaUAilgqpO1yPGdQ5D87nX0LzK5oW2Myova86D5jBvbq9PYAGYFL0pZHn4J2GDg93hvQI4WOn8eVJTXV4Z68i2g7yW8/LjNyr2/sDZMNLa2IAzaHDA9Ooy0M+dEWfM3Pq4sWhKYozHa246TOee3V7e4Aarpj5CzXlkBeJs8EL8rV+tOxX7n7A3NCyr3Z8kxFOurgV41tsj/egBZd+93Gvbu8PoGY10pwNLzSEwdSuK0+uPCD7Wr9j8O5T/vWKQvErTk8hOXhF9Qa3amZxr25vDxBmjBqQ8BrPxFGvIzzSE3DyT23OmFz4R+297p993Kvb+wOkGXzFsMyjfrmiONM0GG9o8pi3lqceHoszrx0r1h7l6IvzOc0HP3szl8a9un37AAyoQZk33lCaonT8M8qz4XZ6zCbf9SSeJ9/iyKs/wb26vT3AczVlIwMZ5LVDfiKYHi11vvlerJz5yuEWwpt7o8c81Z+zi6POWDvQV869un3/AIlnivqQHFAfoppFyhfCm+CAgPGhoa/4DF1Rx5EDasWE73Hfs/EQDT5j5dyr29sDMFwfpnxCzR4jP+ALxj1dmB7VwU1I33HZA/c00Wakz2t5wr9y7tXt/QG+XiYBs0Xyqm1B84lfMxSLV56oGr88ubPqRPk2vfhbTF/0JOc9gHt1e3sAN+2gYU+sVk5deT6iYuSpO/BVDpKjF57ZT9Yh5E0b/gDcQvjNw726vT2AG30wvNcXMX3Uo1fcl6mjNR/55NwHP7mFp1+95mKX8uXjXt3eH0CG1SRjfJiQjSA8iy8PHHxqmhNcRrz0MStq4youRH/mxTHXuRGdr3sWHiuumnt1e3+ANGOcjbNWjDxrdKB56I/J01f5hHlB+QXzMkN+70VDn17u1e3tAaJp0+DDoobzOjnvUT5nCI+Gb/YHnv/APUrePepXvp2x49WzYsxa4F7d3h9AhrUYI41zQXD49KH40Ih5hM8ktjz1HfAVmJmAC2TNzDbXau7V7e0BfIighofZXDwc/AI+LS4NZG/o6WdGeFM/453LGJBuEa1mUTsnL30L3Kvb2wOwvIbusPF4FB6oNl9p1CvS43Xjdhg+zx9o9rP0rRjf4HpGgXt1e3sATBVt6NSA15FruXqsb3rOgD974b9YmH2773HNY+SKzE2ee3X72QPUB1iTPoSBghaICx6favdbPPPMPgA3tczpg5v7Irfa5/l87tXtZw9wNvzp6/yB8tTvSzNIc31q03+0p8WcF3Nd2/kXtKNmLVSfY3Hcq9vbA/CRioDah4SmA1iwYnhUG1qfeaI/UbXiwtTLI818Beesx1HfnB7u1e1nD+BDP1+R5UDc/SfjQ6fHtMjhp9frNXPLJ7xXM30uu7aaIE3zF6Rxr27vD7BEFil+nh8VueJCfWgOC93genisV7g36kL2KQaUm1d99xTxwAurDt5yfPAO7tXt7QHCYMM5joFt6Ijk9FYNh19acvQF0OnJGo055FMjZ05D8uFb4F7d3h5A5rsXGo/4uxnLI45oefELpZs2oV7t2M2/9+XgVx1zyVPH45F7dXt/ABm80XJ9qPMsDi59VSdH/2dZl5aQHtqCotfyV77g+Q6f+S44y6OWZ4F7dXt7ADddQgzOWDm15cUvlN/y+DDjIxpf/ejmafWKNWeh5uMZOvfq9vYAZRy4+3hwGrTi/xRNKx968kD+4uRNf8wR53140fAaJhc5fmohd+HhXt3eHkCGO7mYj2VANNvCWJB86At3UmvA47lx1ZNc1Ju8xYW2a2jKqxcta/Hcq9v7A2QDzYp3vE69PBvedXpn345vmnNgacE96lxBWgL9KOde3d4eQOJtM4Lb9mGBVcuHty3iA1fczRIfWLnrR/nhHEfOlC++xXoUqz957tXt7QFua5AacmAg6xiSUfg0Y3BTXzH01DynX97iM/r8nU4dcWiC9+D5VHlq8Nyr29sD0ExjNGT0RW0J+sPznupdGjXwHurIc3fMNh1cmtM0ccxauAW/OO7V7e0BZNIQzJcieS3iYzO6fktwbgfvU55oPQd8m4+GzyIe7tXt/RdgJuU3M0YzkJ65tImbC80vLnuYJ5Ru81rt/MpjBhje0uAW4jsGh5d7dXv/BcicYCgHKXpePouB7Cme+sEP+S1iegriLb84e+n/nVzibK44m60+7tXt7QFiMWZF/3iQg1qeNR9VH3fQpx3yuE85u0tTnRy+8ILkwn/Ep8YM5dyr29sDhHHhhjXfsDiXk0tTT+nZs/04A3rsdN/IQ6dODn/rM+DnlptfvK65V7e3B6hhZiZGvvji0gOa1z6qeh2aszw+q/UnqrbvifzAzxyHe+jjXt3eHuD6aHBIO+PHwuurDl/yMQ+PYuoO76eneTNGnXxhetN/Nid1fNyr209//MgeIM3VdFAHclnlLFjwpcIneLN2zycCOpz5pN+4f86rn77ZX1h91Zu1cu79241/nU6/+N16hWtXVwLCu46XVx8Erj/8zUnHv7cPsFAPcPrpL69Ov/1T/Dm809DPXv/zOv7l1Qen7wEFDaNAEGAZ7gAAAABJRU5ErkJggg==</pixelData>
+        </body>
+      </item>
+    </_items>
+    <_size dataType="Int">1</_size>
+  </layers>
+</root>
+<!-- XmlFormatterBase Document Separator -->
---
+++ Source/Code/CorePlugin/Gui/TexturedFrame.cs
@@ -0,0 +1,194 @@
+﻿using Duality;
+using Duality.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Duality.Drawing;
+using Duality.Resources;
+using Duality.Components.Renderers;
+
+namespace FellSky.Components.Gui
+{
+    [Duality.Editor.EditorHintCategory("Gui")]
+    public class TexturedFrame : Renderer
+    {
+        public override float BoundRadius => 100;
+
+        public Rect TargetRect {
+            get => _targetRect;
+            set
+            {
+                _targetRect = value;
+            }
+        }
+        public Rect InnerUvSquare {
+            get => _innerUvSquare;
+            set
+            {
+                _innerUvSquare = value;
+                _isUVDirty = true;
+            }
+        }
+        public ContentRef<Material> Background {
+            get => _background;
+            set
+            {
+                _background = value;
+                _isUVDirty = true;
+            }
+        }
+
+        public float VertexZOffset {
+            get => _vertexZOffset;
+            set
+            {
+                _vertexZOffset = value;
+            }
+        }
+
+        public ColorRgba Color { get; set; } = ColorRgba.White;
+
+        private Rect _innerUvSquare = new Rect(0.05f, 0.05f, 0.9f, 0.9f);
+
+        private float _vertexZOffset;
+        private ColorRgba _color;
+        private ContentRef<Material> _background;
+        private Rect _targetRect = new Rect(128,128);
+
+        [DontSerialize]
+        private VertexC1P3T2[] _vertices = new VertexC1P3T2[9 * 4];
+        [DontSerialize]
+        private Vector3[,] _positions = new Vector3[4, 4];
+        [DontSerialize]
+        private Vector2[,] _uvs = new Vector2[4, 4];
+        [DontSerialize]
+        private bool _isUVDirty = true;
+        private bool _isColorDirty = true;
+
+        public override void Draw(IDrawDevice device)
+        {
+            var texture = Background.Res?.MainTexture.Res;
+            if (texture == null)
+                return;
+
+            PrepareVertices(device);
+            device.AddVertices(Background, VertexMode.Quads, _vertices);
+        }
+
+        private void PrepareVertices(IDrawDevice device)
+        {
+            if (_vertices == null || _vertices.Length != (9 * 4))
+                _vertices = new VertexC1P3T2[9 * 4];
+            var tex = Background.Res.MainTexture.Res;
+            if (tex == null)
+                return;
+
+            Vector3 posTemp = this.GameObj.Transform.Pos;
+            float scaleTemp = 1.0f;
+            device.PreprocessCoords(ref posTemp, ref scaleTemp);
+            MathF.GetTransformDotVec(this.GameObj.Transform.Angle, scaleTemp, out var xDot, out var yDot);
+
+            var uvTexSize = tex.Size;
+
+            if (_isUVDirty)
+                RecalculateUVs(tex);
+
+            var cornerLT = _targetRect.TopLeft;
+            var marginLT = _targetRect.TopLeft + _innerUvSquare.TopLeft;
+            var marginBR = _targetRect.BottomRight - (uvTexSize - _innerUvSquare.BottomRight);
+            var cornerBR = _targetRect.BottomRight;
+
+            MathF.TransformDotVec(ref cornerLT, ref xDot, ref yDot);
+            MathF.TransformDotVec(ref cornerBR, ref xDot, ref yDot);
+            MathF.TransformDotVec(ref marginLT, ref xDot, ref yDot);
+            MathF.TransformDotVec(ref marginBR, ref xDot, ref yDot);
+
+            _positions[0, 0] = new Vector3(cornerLT.X, cornerLT.Y, _vertexZOffset) + posTemp;
+            _positions[1, 0] = new Vector3(marginLT.X, cornerLT.Y, _vertexZOffset) + posTemp;
+            _positions[2, 0] = new Vector3(marginBR.X, cornerLT.Y, _vertexZOffset) + posTemp;
+            _positions[3, 0] = new Vector3(cornerBR.X, cornerLT.Y, _vertexZOffset) + posTemp;
+
+            _positions[0, 1] = new Vector3(cornerLT.X, marginLT.Y, _vertexZOffset) + posTemp;
+            _positions[1, 1] = new Vector3(marginLT.X, marginLT.Y, _vertexZOffset) + posTemp;
+            _positions[2, 1] = new Vector3(marginBR.X, marginLT.Y, _vertexZOffset) + posTemp;
+            _positions[3, 1] = new Vector3(cornerBR.X, marginLT.Y, _vertexZOffset) + posTemp;
+
+            _positions[0, 2] = new Vector3(cornerLT.X, marginBR.Y, _vertexZOffset) + posTemp;
+            _positions[1, 2] = new Vector3(marginLT.X, marginBR.Y, _vertexZOffset) + posTemp;
+            _positions[2, 2] = new Vector3(marginBR.X, marginBR.Y, _vertexZOffset) + posTemp;
+            _positions[3, 2] = new Vector3(cornerBR.X, marginBR.Y, _vertexZOffset) + posTemp;
+
+            _positions[0, 3] = new Vector3(cornerLT.X, cornerBR.Y, _vertexZOffset) + posTemp;
+            _positions[1, 3] = new Vector3(marginLT.X, cornerBR.Y, _vertexZOffset) + posTemp;
+            _positions[2, 3] = new Vector3(marginBR.X, cornerBR.Y, _vertexZOffset) + posTemp;
+            _positions[3, 3] = new Vector3(cornerBR.X, cornerBR.Y, _vertexZOffset) + posTemp;
+
+            int index = 0;
+            for (int y = 0; y < 3; y++)
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    _vertices[index++] = new VertexC1P3T2
+                    {
+                        Color = _color,
+                        Pos = _positions[x, y],
+                        TexCoord = _uvs[x, y]
+                    };
+                    _vertices[index++] = new VertexC1P3T2
+                    {
+                        Color = _color,
+                        Pos = _positions[x, y+1],
+                        TexCoord = _uvs[x, y+1]
+                    };
+                    _vertices[index++] = new VertexC1P3T2
+                    {
+                        Color = _color,
+                        Pos = _positions[x+1, y+1],
+                        TexCoord = _uvs[x+1, y+1]
+                    };
+                    _vertices[index++] = new VertexC1P3T2
+                    {
+                        Color = _color,
+                        Pos = _positions[x+1, y],
+                        TexCoord = _uvs[x+1, y]
+                    };
+                }
+            }
+        }
+
+        private void RecalculateUVs(Texture tex)
+        {
+            if (tex == null)
+                return;
+
+            var uvRatio = tex.UVRatio;
+            var uvTexSize = tex.Size;
+
+            var uvCornerLT = Vector2.Zero;
+            var uvMarginLT = uvRatio * _innerUvSquare.TopLeft / uvTexSize;
+            var uvMarginBR = uvRatio * _innerUvSquare.BottomRight / uvTexSize;
+            var uvCornerBR = uvRatio;
+
+            _uvs[0, 0] = new Vector2(uvCornerLT.X, uvCornerLT.Y);
+            _uvs[1, 0] = new Vector2(uvMarginLT.X, uvCornerLT.Y);
+            _uvs[2, 0] = new Vector2(uvMarginBR.X, uvCornerLT.Y);
+            _uvs[3, 0] = new Vector2(uvCornerBR.X, uvCornerLT.Y);
+            _uvs[0, 1] = new Vector2(uvCornerLT.X, uvMarginLT.Y);
+            _uvs[1, 1] = new Vector2(uvMarginLT.X, uvMarginLT.Y);
+            _uvs[2, 1] = new Vector2(uvMarginBR.X, uvMarginLT.Y);
+            _uvs[3, 1] = new Vector2(uvCornerBR.X, uvMarginLT.Y);
+            _uvs[0, 2] = new Vector2(uvCornerLT.X, uvMarginBR.Y);
+            _uvs[1, 2] = new Vector2(uvMarginLT.X, uvMarginBR.Y);
+            _uvs[2, 2] = new Vector2(uvMarginBR.X, uvMarginBR.Y);
+            _uvs[3, 2] = new Vector2(uvCornerBR.X, uvMarginBR.Y);
+            _uvs[0, 3] = new Vector2(uvCornerLT.X, uvCornerBR.Y);
+            _uvs[1, 3] = new Vector2(uvMarginLT.X, uvCornerBR.Y);
+            _uvs[2, 3] = new Vector2(uvMarginBR.X, uvCornerBR.Y);
+            _uvs[3, 3] = new Vector2(uvCornerBR.X, uvCornerBR.Y);
+        }
+    }
+
+
+}
