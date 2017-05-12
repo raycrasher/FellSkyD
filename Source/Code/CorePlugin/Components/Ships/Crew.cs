7543e46df94c64c9e3c9541eb454132bb96659a1
Ian David Garcia <cinquedia@gmail.com>
2017-05-12 16:39:09 +0800

    Added crew and species placeholders

---
+++ Source/Code/CorePlugin/Components/Ships/Crew.cs
@@ -0,0 +1,19 @@
+﻿using Duality;
+using FellSky.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FellSky.Components.Ships
+{
+    public class Crew: Component
+    {
+        public string Name { get; set; }
+        public string Nickname { get; set; }
+        public TimeSpan Age { get; set; }
+        public ContentRef<SpeciesType> Species { get; set; }
+        public ContentRef<Sprite> Portrait { get; set; }
+    }
+}
