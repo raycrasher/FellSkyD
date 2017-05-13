8bd426c9ffc250fdabb5a38b0fe8baa06639b358
Ian David Garcia <cinquedia@gmail.com>
2017-05-13 12:00:45 +0800

    Added working particle system

---
+++ Source/Code/CorePlugin/Components/Projectiles/BeamRenderer.cs
@@ -0,0 +1,26 @@
+﻿using Duality.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Duality.Drawing;
+using Duality;
+using FellSky.Resources;
+
+namespace FellSky.Components.Projectiles
+{
+    [Duality.Editor.EditorHintCategory("Graphics")]
+    public class BeamRenderer : Renderer
+    {
+        public override float BoundRadius => 100;
+
+        public Transform Source { get; set; }
+        public ContentRef<Sprite>[] Sprites { get; set; }
+
+        public override void Draw(IDrawDevice device)
+        {
+            
+        }
+    }
+}
