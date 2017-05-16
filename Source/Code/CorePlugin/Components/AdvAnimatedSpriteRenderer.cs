058967142f5fd3c2d8169197fe4b2c46dfc87ec4
Ian David Garcia <cinquedia@gmail.com>
2017-05-16 18:30:20 +0800

    Added AdvAnimatedSpriteRenderer

--- Source/Code/CorePlugin/Components/AdvAnimatedSpriteRenderer.cs
+++ Source/Code/CorePlugin/Components/AdvAnimatedSpriteRenderer.cs
@@ -11,14 +11,96 @@ using FellSky.Resources;
 namespace FellSky.Components
 {
     [Duality.Editor.EditorHintCategory("Graphics")]
-    public class AdvAnimatedSpriteRenderer : Renderer
-    {
-        public ContentRef<Sprite>[] Frames { get; set; }
+    public class AdvAnimatedSpriteRenderer : AdvSpriteRenderer
+    {         
+        public enum LoopMode
+        {
+            Once, Loop, PingPong, Fixed
+        }
+
+        public class Frame
+        {
+            public ContentRef<Sprite> Sprite { get; set; }
+            public float Delay { get; set; } = 200;
+        }
+
+        public Frame[] Frames { get; set; }
         public override float BoundRadius => throw new NotImplementedException();
+
+        public LoopMode Mode { get; set; }
+        public int CurrentFrame {
+            get => _currentFrame;
+            set => _currentFrame = MathF.Clamp(0, 0, Frames?.Length ?? 0);
+        }
+
+        private float _timer = 0;
+        private bool _pingPongDir; //false = back, true = forward
+        private int _currentFrame;
 
         public override void Draw(IDrawDevice device)
         {
-            throw new NotImplementedException();
+            if (Frames == null || Frames.Length <= 0)
+                return;
+
+            switch (Mode)
+            {
+                case LoopMode.Fixed:
+                    return;
+                case LoopMode.Loop:
+                    if(_timer >= Frames[CurrentFrame].Delay)
+                    {
+                        _timer = _timer - Frames[CurrentFrame].Delay;
+                        CurrentFrame++;
+                        break;
+                    }
+                    _timer += Time.TimeMult;
+                    break;
+                case LoopMode.Once:
+                    if (CurrentFrame < Frames.Length - 1)
+                    {
+                        if (_timer >= Frames[CurrentFrame].Delay)
+                        {
+                            _timer = _timer - Frames[CurrentFrame].Delay;
+                            CurrentFrame++;
+                            break;
+                        }
+                        _timer += Time.TimeMult;
+                    }
+                    break;
+                case LoopMode.PingPong:
+
+                    if (_timer >= Frames[CurrentFrame].Delay)
+                    {
+                        if (_pingPongDir)
+                        {
+                            if (CurrentFrame + 1 > Frames.Length)
+                            {
+                                _timer = _timer - Frames[CurrentFrame].Delay;
+                                _pingPongDir = false;
+                                CurrentFrame--;
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            if (CurrentFrame - 1 < 0)
+                            {
+                                _timer = _timer - Frames[CurrentFrame].Delay;
+                                _pingPongDir = true;
+                                CurrentFrame++;
+                                break;
+                            }
+                        }
+                        CurrentFrame++;
+                    }
+                    else
+                    {
+                        _timer += Time.TimeMult;
+                    }
+                    break;
+            }
+
+            Sprite = Frames[CurrentFrame].Sprite;
         }
     }
 }
